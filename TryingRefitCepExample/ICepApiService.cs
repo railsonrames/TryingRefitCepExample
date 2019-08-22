31a08b8a8d30ff9f6e353efba5c2740de7e5a130
Railson Rames <rames.cloud@outlook.com.br>
2019-08-22 17:32:16 -0300

    Add project files.

---
+++ TryingRefitCepExample/ICepApiService.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingRefitCepExample
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressByCepAsync(string cep);
+    }
+}
---
+++ TryingRefitCepExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace TryingRefitCepExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+            string searchedCep = "72250316";
+            Console.WriteLine($"Consultado o CEP: {searchedCep}");
+            var retrievedAddress = await cepClient.GetAddressByCepAsync(searchedCep);
+            Console.WriteLine($"\nCEP: {retrievedAddress.Cep}\nLogradouro: {retrievedAddress.Logradouro}\nComplemento: {retrievedAddress.Complemento}\nBairro: {retrievedAddress.Bairro}\nLocalidade: {retrievedAddress.Localidade}\nEstado: {retrievedAddress.Uf}\nCódigo Ibge: {retrievedAddress.Ibge}\nGia: {retrievedAddress.Gia}");
+            Console.ReadKey();
+        }
+    }
+}
